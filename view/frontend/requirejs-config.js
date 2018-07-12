0405f6bb6c1d41adc2624ef645494a3e4d22fee4
Livia Baltatescu <livia@eadesign.ro>
2018-07-12 15:18:56 +0300

    #2448: I added js for city dropdown. Is not complete.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -13,6 +13,9 @@ var config = {
         mixins: {
             'Magento_Checkout/js/model/shipping-rates-validation-rules': {
                 'Eadesigndev_RomCity/js/model/shipping-rates-validation-rules-mixin': true
+            },
+            'Magento_Checkout/js/action/set-shipping-information': {
+                'Eadesigndev_RomCity/js/action/set-shipping-information-mixin': true
             }
         }
     }
